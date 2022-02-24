774b52c574ff8eac165cbb87a5919c29c84ae5d7
Gus Bikos <gusbikos@gmail.com>
2022-02-23 19:52:58 -0500

    Complete

--- completed-assessment.js
+++ completed-assessment.js
@@ -1,8 +1,12 @@
+// Create a 3x3 grid on the X & Y axis and randomize the numbers
+
 const generateSquare = () => {
     const square = []
     for (let x = 0; x < 3; x++) {
         square[x] = []
         for (let y = 0; y < 3; y++) {
+            // Make sure the center cell is set to 0 everything other cell is random
+
             const centerCell = square[x][1] = 0
             square[x][y] = Number(Math.random() < 0.5)
         }
@@ -10,12 +14,16 @@ const generateSquare = () => {
     return square
 }
 
+// Take the square object that was created in the previous function and flatten it into a single array to compare elements much easier.
+
 const countCells = (square) => {
     let count = 0
     const flattened = square.flat()
 
+
     for (let i = 1; i < flattened.length; i++) {
         if ( flattened[i] == 0) {
+            // If the number at that index = 0 which is what the center cell is then we increment the counter + 1
             count++
         }
     }    
@@ -23,6 +31,6 @@ const countCells = (square) => {
 }
 
 const square = generateSquare()
-console.table(square)
+console.table(square) // Create a visual table in the console
 console.log(countCells(square))
 
